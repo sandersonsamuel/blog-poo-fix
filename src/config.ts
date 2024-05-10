e320d13221e2e141ea0299de63179369494abc92
sandersonsamuel <sandersonsamuel401@gmail.com>
2024-05-09 22:25:49 -0300

    fix: resolvendo bug de deploy

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Introdução à Programação Orientada a Objetos com Java";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "Introdução à Programação Orientada a Objetos com Java";
+export const TWITTER_HANDLE = "@unifacema";
+export const MY_NAME = "Ads 2023.2";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
